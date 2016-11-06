2b16375f172d42b30147e0a201f951ffd8d76949
jborza <jborza@gmail.com>
2016-11-06 10:28:04 +0100

    Added a unit test for StreamEstimator

---
+++ CompressionEstimator.Tests/StreamEstimatorTests.cs
@@ -0,0 +1,29 @@
+﻿using LibCompressionEstimator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace CompressionEstimator.Tests
+{
+    [TestClass]
+    public class StreamEstimatorTests
+    {
+        [TestMethod]
+        public void EstimatePackedSizeTest()
+        {
+            //Arrange
+            var se = new StreamEstimator();
+            var stream = CreateSampleCompressibleStream(1024 * 1024);
+            //Act
+            var result = se.EstimatePackedSize(stream, 1024 * 100, 1024);
+            //Assert
+            var expectedLength = 4741; //validated once manually
+            Assert.AreEqual(expectedLength, result);
+        }
+
+        private MemoryStream CreateSampleCompressibleStream(int bytes)
+        {
+            var byteArray = new byte[bytes];
+            return new MemoryStream(byteArray, writable: false);
+        }
+    }
+}
--- LibCompressionEstimator/StreamEstimator.cs
+++ LibCompressionEstimator/StreamEstimator.cs
@@ -3,11 +3,18 @@ using System.IO.Compression;
 
 namespace LibCompressionEstimator
 {
-    class StreamEstimator
+    internal class StreamEstimator
     {
-        public long EstimatePackedSize(Stream inputStream, int maxReadSize, int blockSize)
+        /// <summary>
+        /// Estimates how large the directory would be after compression,  
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="sampleStreamSize">A maximum length of the directory sample stream</param>
+        /// <param name="blockSize">Contiguous block size to read from the source stream</param>
+        /// <returns></returns>
+        public long EstimatePackedSize(Stream inputStream, int sampleStreamSize, int blockSize)
         {
-            var config = PrepareConfiguration(inputStream, maxReadSize, blockSize);
+            var config = PrepareConfiguration(inputStream, sampleStreamSize, blockSize);
             double estimatedCompressionRatio = EstimateCompressionRatio(inputStream, config);
             return (long)(estimatedCompressionRatio * config.InputLength);
         }
@@ -18,24 +25,26 @@ namespace LibCompressionEstimator
             long compressedBytes;
             byte[] buffer = new byte[config.BlockSize];
             using (MemoryStream ms = new MemoryStream())
-            using (var compressedStream = CreateCompressedStream(ms))
             {
-                for (int i = 0; i < config.Blocks; i++)
+                using (var compressedStream = CreateCompressedStream(ms))
                 {
-                    var read = inputStream.Read(buffer, 0, config.BlockSize);
-                    compressedStream.Write(buffer, 0, read);
-                    totalReadBytes += read;
-                    inputStream.Seek(config.SkipOffsetBetweenBlocks, SeekOrigin.Current);
+                    for (int i = 0; i < config.Blocks; i++)
+                    {
+                        var read = inputStream.Read(buffer, 0, config.BlockSize);
+                        compressedStream.Write(buffer, 0, read);
+                        totalReadBytes += read;
+                        inputStream.Seek(config.SkipOffsetBetweenBlocks, SeekOrigin.Current);
+                    }
                 }
                 compressedBytes = ms.Length;
             }
-            double estimatedCompressionRatio = (float)compressedBytes / totalReadBytes;
+            double estimatedCompressionRatio = (double)compressedBytes / totalReadBytes;
             return estimatedCompressionRatio;
         }
 
         private static Stream CreateCompressedStream(Stream stream)
         {
-            return new DeflateStream(stream, CompressionLevel.Fastest);
+            return new DeflateStream(stream, CompressionLevel.Fastest, true);
         }
 
         private StreamEstimatorConfig PrepareConfiguration(Stream inputStream, int maxReadSize, int blockSize)
